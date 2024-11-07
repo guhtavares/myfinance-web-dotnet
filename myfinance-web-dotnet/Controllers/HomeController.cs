c932f8ae2cb2e6a9357cd69923f00c55d03ad733
gusta <gustavo.tavares2801@gmail.com>
2024-11-06 23:33:01 -0300

    Criando conexao com o banco de dados e apresentando o primeiro campo em tela.

--- myfinance-web-dotnet/Controllers/HomeController.cs
+++ myfinance-web-dotnet/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using myfinance_web_dotnet.Infrastructure;
 using myfinance_web_dotnet.Models;
 
 namespace myfinance_web_dotnet.Controllers;
@@ -7,14 +8,20 @@ namespace myfinance_web_dotnet.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private MyFinanceDbContext _myFinanceDbContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, 
+       MyFinanceDbContext MyFinanceDbContext)
     {
         _logger = logger;
+       _myFinanceDbContext = MyFinanceDbContext;
     }
 
     public IActionResult Index()
     {
+        var teste = _myFinanceDbContext.PlanoConta.FirstOrDefault();
+        ViewBag.TESTE = teste.nome;
+        
         return View();
     }
 
---
+++ myfinance-web-dotnet/Domain/PlanoConta.cs
@@ -0,0 +1,8 @@
+﻿namespace myfinance_web_dotnet.Domain;
+
+public class PlanoConta
+{
+    public int id { get; set; }
+    public string nome { get; set; }
+    public char tipo { get; set; }
+}
